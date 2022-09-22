9978023e2fa815eb14c79987341de5f31de2fab1
wilshan <74950445+wilshan@users.noreply.github.com>
2022-09-22 20:14:43 +0530

    Create console_userdata.sh

---
+++ console_userdata.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -xe
+sudo yum update -y
+sudo yum install git -y
+sudo amazon-linux-extras install docker -y
+sudo service docker start
+sudo systemctl enable docker
+sudo usermod -a -G docker ec2-user
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+cd /home/ec2-user
+git clone https://github.com/TheCloudWorld/userdata-application-install.git
+cd userdata-application-install/docker-compose/console
+docker-compose up -d
