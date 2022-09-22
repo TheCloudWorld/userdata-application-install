91559d24810954a855e4e7f704d47c0a6c8ab815
wilshan <74950445+wilshan@users.noreply.github.com>
2022-09-22 19:40:09 +0530

    Update docker_userdata.sh

--- docker_userdata.sh
+++ docker_userdata.sh
@@ -4,3 +4,5 @@ sudo amazon-linux-extras install docker -y
 sudo service docker start
 sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
