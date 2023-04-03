0010ea201d9c661776a3aa835b29f63b6e291c5b
TheCloudWorld <74950445+TheCloudWorldAdmin@users.noreply.github.com>
2023-04-03 12:43:20 +0530

    Update jenkins_userdata.sh

--- jenkins_userdata.sh
+++ jenkins_userdata.sh
@@ -1,9 +1,9 @@
-#!/bin/bash -xe
+#!/bin/bash
 yum update -y
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum upgrade -y
-amazon-linux-extras install java-openjdk11 -y
+sudo yum install java-11-amazon-corretto-headless
 yum install jenkins -y
 systemctl daemon-reload
 systemctl enable jenkins
